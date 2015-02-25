b79b7ff5e2358935305274a8db9fa6045bc6d6e5
marthy123mm <loscuisillos@hotmail.com>
2015-02-25 10:34:02 -0600

    Se Empezo el proyecto y se agrego un metodo para el Cronometro

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Threading;
+
+namespace Practica2
+{
+	class Program
+	{
+		public static void cronometro(){
+			int horas=0;
+			int minutos=0;
+			int segundos=0;
+			
+			for (horas=0; horas <24; horas ++)
+				for(minutos=0;  minutos<60; minutos ++)
+					for(segundos=0; segundos<60; segundos++){
+						Console.WriteLine(horas + " : " + minutos + " : " + segundos);
+						Thread.Sleep(1000);
+						Console.Clear();
+					}
+						
+		}
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
